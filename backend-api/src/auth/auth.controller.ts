6ceb166cd6db0cc2a514b3d2882980824ea583ee
jin <jin@sellmate.co.kr>
2025-04-10 01:06:49 +0900

    스웨거 수정, 비밀번호 찾기 API 추가

--- backend-api/src/auth/auth.controller.ts
+++ backend-api/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/createUser.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FindPasswordDto } from './dto/find-password.dto';
 
 @ApiTags('인증 API')
 @Controller('auth')
@@ -14,9 +15,82 @@ export class AuthController {
     description: '회원정보를 추가합니다.',
   })
   @ApiBody({ type: CreateUserDto })
-  @ApiResponse({ status: 201, description: '회원가입에 성공하였습니다.' })
-  @ApiResponse({ status: 404, description: '회원가입에 실패하였습니다.' })
-  signup(@Body() createUserDto: CreateUserDto) {
-    return this.authService.signup(createUserDto);
+  @ApiResponse({
+    status: 201,
+    description: '회원가입에 성공하였습니다.',
+    schema: {
+      properties: {
+        status: {
+          type: 'boolean',
+          description: 'API 호출 성공 여부',
+          example: 201,
+        },
+        message: {
+          type: 'string',
+          description: 'API 호출 메세지',
+          example: 'API 호출에 성공하였습니다.',
+        },
+        data: {
+          type: 'object',
+          description: 'API 응답값',
+          example: {
+            id: 1, // 생성된 user Id
+            email: 'test@test.com', // 생성된 user email
+            name: '홍길동', //  생성된 user 이름
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 409,
+    schema: {
+      properties: {
+        status: {
+          type: 'number',
+          example: 409,
+        },
+        message: {
+          type: 'string',
+          example: '이미 존재하는 이메일입니다.',
+        },
+        errorCode: {
+          type: 'string',
+          example: 'user already exists',
+        },
+      },
+    },
+  })
+  @ApiResponse({ status: 500, description: '회원가입에 실패하였습니다.' })
+  async signup(@Body() createUserDto: CreateUserDto) {
+    const user = await this.authService.signup(createUserDto);
+
+    return {
+      status: 201,
+      message: '회원가입에 성공하였습니다.',
+      data: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    };
+  }
+
+  @Post('find-password')
+  @ApiOperation({
+    summary: '비밀번호 찾기 API',
+    description: '비밀번호를 찾습니다.',
+  })
+  @ApiBody({ type: FindPasswordDto })
+  async findPassword(@Body() findPasswordDto: FindPasswordDto) {
+    const password = await this.authService.findPassword(findPasswordDto);
+
+    return {
+      status: 201,
+      message: '비밀번호 찾기에 성공하였습니다.',
+      data: {
+        password,
+      },
+    };
   }
 }
--- backend-api/src/auth/auth.service.ts
+++ backend-api/src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './user.entity';
+import { User } from './entity/user.entity';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/createUser.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { FindPasswordDto } from './dto/find-password.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,16 +13,74 @@ export class AuthService {
   ) {}
 
   async signup(createUserDto: CreateUserDto): Promise<User> {
-    const { email, password, name } = createUserDto;
+    try {
+      const { email, password, name } = createUserDto;
+      const exists = await this.usersRepository.findOneBy({
+        email,
+      });
 
-    // const hashedPassword = await bcrypt.hash(password, 10);
+      if (exists) {
+        throw new ConflictException({
+          status: 409,
+          message: '이미 존재하는 이메일입니다.',
+          errorCode: 'user already exists',
+        });
+      }
 
-    const user = this.usersRepository.create({
-      email,
-      password /* : hashedPassword */,
-      name,
-    });
+      if (name.length === 0) {
+        throw new ConflictException({
+          status: 409,
+          message: '이름은 필수 값입니다.',
+          errorCode: 'user name is required',
+        });
+      }
+      // const hashedPassword = await bcrypt.hash(password, 10);
 
-    return this.usersRepository.save(user);
+      const user = this.usersRepository.create({
+        email,
+        password /* : hashedPassword */,
+        name,
+      });
+
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      const { code, message } = error as unknown as {
+        code: number;
+        message: string;
+      };
+      throw new ConflictException({
+        status: code ?? 500,
+        message: message ?? '오류가 발생했습니다.',
+      });
+    }
+  }
+
+  async findPassword(findPasswordDto: FindPasswordDto) {
+    try {
+      const { email, name } = findPasswordDto;
+      const user = await this.usersRepository.findOneBy({
+        email,
+        name,
+      });
+
+      if (user) {
+        return user.password;
+      } else {
+        throw new ConflictException({
+          status: 409,
+          message: '일치하는 유저가 없습니다1.',
+          errorCode: 'user not found',
+        });
+      }
+    } catch (error) {
+      const { code, message } = error as unknown as {
+        code: number;
+        message: string;
+      };
+      throw new ConflictException({
+        status: code ?? 500,
+        message: message ?? '오류가 발생했습니다.',
+      });
+    }
   }
 }
