ae9d3c75b12331099c10150c8f935c2ec496f8ec
yang <diddnjswns20@gmail.com>
2025-04-24 00:20:17 +0900

    change

--- src/login/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from "class-validator";
-
-export class LoginDto {
-  @IsEmail({}, { message: "Invalid email format" })
-  email: string;
-
-  @IsNotEmpty({ message: "Password is required" })
-  password: string;
-}
