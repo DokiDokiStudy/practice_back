9b99f9610c5a32f71d19d94974d1c710cd026d33
박세영 <caiso2978@naver.com>
2025-05-06 20:20:06 +0900

    jwt 지속 시간 변경

--- backend-api/src/auth/auth.module.ts
+++ backend-api/src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'defaultSecret',
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '3h' },
     }),
     TypeOrmModule.forFeature([User]),
   ],
