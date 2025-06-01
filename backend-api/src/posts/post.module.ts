0849fec7f4a5e6179c9da8242cd5a37d3d96c96f
yang <diddnjswns20@gmail.com>
2025-06-02 00:51:14 +0900

    복수형 -> 단수형 네이밍

--- backend-api/src/posts/post.module.ts
+++ backend-api/src/posts/post.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Posts } from './entities/posts.entity';
+import { Posts } from './entities/post.entity';
 import { User } from 'src/users/entities/user.entity';
 
 @Module({
--- backend-api/src/users/entities/user.entity.ts
+++ backend-api/src/users/entities/user.entity.ts
@@ -1,62 +1,56 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Posts } from 'src/posts/entities/posts.entity';
-import { Likes } from 'src/likes/entities/likes.entity';
-import { Comments } from 'src/comments/entities/comments.entity';
 import {
   Entity,
-  Column,
   PrimaryGeneratedColumn,
-  DeleteDateColumn,
-  UpdateDateColumn,
+  Column,
   CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
   OneToMany,
+  Index,
 } from 'typeorm';
+import { Post } from 'src/posts/entities/post.entity';
+import { Comment } from 'src/comments/entities/comment.entity';
+import { Like } from 'src/likes/entities/like.entity';
 
 @Entity()
 export class User {
-  @OneToMany(() => Posts, (post) => post.user)
-  posts: Posts[];
-  @OneToMany(() => Likes, (like) => like.user)
-  likes: Likes[];
-  @OneToMany(() => Comments, (comment) => comment.user)
-  comments: Comments[];
-
-  @ApiProperty({ example: 1 })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ example: 'test@gmail.com' })
-  @Column({ unique: true })
+  @Index()
+  @Column({ unique: true, length: 255 })
   email: string;
 
-  @ApiProperty({ example: 'password' })
-  @Column()
+  @Column({ length: 255 })
   password: string;
 
-  @ApiProperty({ example: 'name' })
-  @Column()
+  @Column({ length: 255 })
   name: string;
 
-  @ApiProperty({ example: 'nickname' })
-  @Column()
+  @Column({ length: 255 })
   nickName: string;
 
-  @ApiProperty({ example: 'true' })
-  @Column()
+  @Column({ default: true })
   isActive: boolean;
 
-  @ApiProperty({ example: '최고 관리자' })
-  @Column()
+  @Column({ length: 255 })
   role: string;
 
-  @ApiProperty({ example: '2025-04-15' })
   @CreateDateColumn()
   createdAt: Date;
 
-  @ApiProperty({ example: '2025-04-15' })
   @UpdateDateColumn()
   updatedAt: Date;
 
-  @DeleteDateColumn()
+  @DeleteDateColumn({ nullable: true })
   deletedAt: Date;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+
+  @OneToMany(() => Like, (like) => like.user)
+  likes: Like[];
 }
