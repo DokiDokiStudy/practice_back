cfc653b474a851b2a5fead0bb3ef3a3c1b0552ce
yang <diddnjswns20@gmail.com>
2025-06-25 19:16:56 +0900

    코멘트 단일 조회 추가

--- src/comments/comment.controller.ts
+++ src/comments/comment.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
   UseGuards,
   Request,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -22,35 +23,40 @@ export class CommentController {
   @UseGuards(AuthGuard('jwt'))
   @Post()
   create(
-    @Request() reqest: AuthRequest,
+    @Request() request: AuthRequest,
     @Body() createCommentDto: CreateCommentDto,
   ) {
-    return this.commentService.create(reqest, createCommentDto);
+    return this.commentService.create(request, createCommentDto);
   }
 
+  // 댓글 전체 조회가 어디 쓰이지
   // @Get()
   // findAll() {
   //   return this.commentService.findAll();
   // }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.commentService.findOne(+id);
-  // }
+  @Get(':commentId')
+  @UseGuards(AuthGuard('jwt'))
+  async findOne(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Request() req: AuthRequest,
+  ) {
+    return this.commentService.findOne(commentId, req.user.id);
+  }
 
   @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
   update(
-    @Request() reqest: AuthRequest,
+    @Request() request: AuthRequest,
     @Param('id') id: string,
     @Body() updateCommentDto: UpdateCommentDto,
   ) {
-    return this.commentService.update(reqest, +id, updateCommentDto);
+    return this.commentService.update(request, +id, updateCommentDto);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
-  remove(@Request() reqest: AuthRequest, @Param('id') id: string) {
-    return this.commentService.remove(reqest, +id);
+  remove(@Request() request: AuthRequest, @Param('id') id: string) {
+    return this.commentService.remove(request, +id);
   }
 }
